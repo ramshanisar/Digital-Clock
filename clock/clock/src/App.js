4c735fcd697b6f21646715e4f568da9383cb2c3f
ramshanisar <ramshanisar642@gmail.com>
2023-03-30 20:45:02 +0500

    Digital Clock

---
+++ clock/clock/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import './App.css';
+const App = () => {
+  let time = new Date().toLocaleTimeString();
+
+  const[ctime, setCtime]=useState(time);
+
+  const UpdateTime = () =>{
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  }
+  setInterval(UpdateTime,1000);
+
+  return (
+    <>
+    <h1 className='clock'>Digital Clock</h1>
+    {ctime}
+    </>
+  )
+}
+
+export default App
